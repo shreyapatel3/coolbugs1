01368c47e0b0dccb59e475fd349a5585158b91e7
Sachin Saxena <sachin212saxena@gmail.com>
2019-10-10 10:38:15 +0530

    user hard code

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class UserComponent implements OnInit{
+
+    userDetail: UserDetail;
+
     ngOnInit(){
+        this.userDetail = new UserDetail();
+        this.userDetail.country = "India";
+        this.userDetail.userName = "Aaditya Pandilwar";
+        this.userDetail.userEmail = "aaditya.pandilwar@db.com";
     }
+}
+
+class UserDetail {
+    userName: string;
+    userEmail: string;
+    country: string;
+
+    constructor() {}
 }
